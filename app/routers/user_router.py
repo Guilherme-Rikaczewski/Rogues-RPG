3f55912ca44043e8022bcd2cc987c8d4e84825b6
Guilherme C. Rikaczewski <guilhermecrikaczewski@gmail.com>
2026-03-12 16:57:14 -0300

    adiciona crud de usuarios

--- app/routers/user_router.py
+++ app/routers/user_router.py
@@ -2,11 +2,43 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from app.db.session import get_db
-from app.schemas.user_schema import UserCreate, UserResponse
-from app.services.user_service import create_user
+from app.schemas.user_schema import UserCreate, UserUpdate, UserResponse
+from app.services.user_service import (create_user, update_user,
+                                       get_user, delete_user)
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
+
 @router.post("/", response_model=UserResponse)
 def create(user: UserCreate, db: Session = Depends(get_db)):
-    return create_user(db, user)
+    try:
+        return create_user(db, user)
+    except Exception:
+        return HTTPException(500, detail='Internal server error')
+
+
+@router.patch('/{id}', response_model=UserResponse)
+def update(id: int, user: UserUpdate, db: Session = Depends(get_db)):
+    try:
+        return update_user(db, id, user)
+    except Exception:
+        return HTTPException(500, detail='Internal server error')
+
+
+@router.get('/{id}', response_model=UserResponse)
+def read(id: int, db: Session = Depends(get_db)):
+    try:
+        return get_user(db, id)
+    except Exception:
+        return HTTPException(500, detail='Internal server error')
+
+
+@router.delete('/{id}', status_code=204)
+def delete(id: int, db: Session = Depends(get_db)):
+    try:
+        success = delete_user(db, id)
+
+        if not success:
+            raise
+    except Exception:
+        return HTTPException(500, detail='Internal server error')
--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -1,24 +1,25 @@
 from pydantic import BaseModel, EmailStr
 from datetime import datetime
+from app.schemas.types import Username, Password
 
 
 class UserCreate(BaseModel):
     email: EmailStr
-    username: str
-    password: str
+    username: Username
+    password: Password
 
 
 class UserUpdate(BaseModel):
     email: EmailStr | None = None
-    username: str | None = None
-    password: str | None = None
+    username: Username | None = None
+    password: Password | None = None
 
 
 class UserResponse(BaseModel):
     id: int
     email: EmailStr
-    username: str
+    username: Username
     created_at: datetime
     updated_at: datetime
-    
+
     model_config = {'from_attributes': True}
