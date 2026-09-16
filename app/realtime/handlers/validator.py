ab9fbeb8c3f46174ad3acd5a856b3f644592b8d8
Guilherme C. Rikaczewski <guilhermecrikaczewski@gmail.com>
2026-09-15 21:20:12 -0300

    feat: adiciona função para validar models

---
+++ app/realtime/handlers/validator.py
@@ -0,0 +1,29 @@
+from app.schemas.tabletop_schema import (
+    AssetMoveMessage,
+    AssetChangeLayerMessage,
+    DiceRollMessage,
+    ChatMessage,
+    WebSocketMessage
+)
+from pydantic import ValidationError
+
+
+MESSAGE_TYPES = {
+    "asset.move": AssetMoveMessage,
+    "asset.change_layer": AssetChangeLayerMessage,
+    "dice.roll": DiceRollMessage,
+    "chat.message": ChatMessage,
+}
+
+
+def validate(
+    data
+) -> WebSocketMessage | None:
+    data_type = data.get("type")
+    schema: WebSocketMessage = MESSAGE_TYPES.get(data_type) # type: ignore
+
+    try:
+        validated_data = schema.model_validate(data)
+        return validated_data
+    except ValidationError:
+        return None
